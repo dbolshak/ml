127816279de92082dab1c0f55269c667a4eefd5a
dbolshak <bolshakov.denis@gmail.com>
2017-01-22 23:36:28 +0300

    updating python-scripts/load_iris.py with example of iris_dataset

--- python-scripts/load_iris.py
+++ python-scripts/load_iris.py
@@ -2,3 +2,22 @@ from sklearn.datasets import load_iris
 
 iris_dataset = load_iris()
 
+'''
+The data itself is contained in the target and data fields. data contains the numeric measurements of sepal length, sepal width, petal length, and petal width in a NumPy array
+'''
+print("dir on iris_dataset: \n{}".format(dir(iris_dataset)))
+print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
+print("Value of the DESCR key:\n{}".format(iris_dataset['DESCR'][:193] + "\n..."))
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Feature names: \n{}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(type(iris_dataset['data'])))
+print("Type of target: {}".format(type(iris_dataset['target'])))
+
+'''
+The rows in the data array correspond to flowers, while the columns represent the four measurements that were taken for each flower:
+'''
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+print("Here are the feature values for the first five samples(five columns of data):\n{}".format(iris_dataset['data'][:5]))
+print("Target is a one-dimensional array, with one entry per flower.Shape of target: {}".format(iris_dataset['target'].shape))
+print("Target:\n{}".format(iris_dataset['target']))
+
